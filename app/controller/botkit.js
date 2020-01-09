482f71014d902786d878d01c628f12f44b0e5803
hcyhehe <960696163@qq.com>
2020-01-09 23:25:30 +0800

    666

--- app/controller/botkit.js
+++ app/controller/botkit.js
@@ -7,7 +7,22 @@ const controller = new Botkit({
     // ...other options
 })
 
-controller.hears('hello','direct_message', function(bot, message) {
-    bot.reply(message,'Hello yourself!')
+
+controller.hears(new RegExp(/^\d+$/), ['message', 'direct_message'], async function(bot, message) {
+    await bot.reply(message, { text: '我懂了，您输入的是数字哦' })
+})
+
+controller.hears(new RegExp('作业'), 'message', async (bot, message) => {
+    await bot.reply(message, {
+        text: '1.这是第一题，以下哪些答案是正确的？',
+        quick_replies: [
+            { title: 'A 是', payload: 'A' },
+            { title: 'B 否', payload: 'B' },
+            { title: 'C 不清楚', payload: 'C' },
+            { title: 'D 其他', payload: 'D' },
+        ]
+    }, function(){
+        console.log('hehe')
+    })
 })
 
