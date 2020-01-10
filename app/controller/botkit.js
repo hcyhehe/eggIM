9098857dea96d189607bd0288001e3b12ab5bd28
hcyhehe <960696163@qq.com>
2020-01-10 10:46:56 +0800

    完善聊天流程

--- app/controller/botkit.js
+++ app/controller/botkit.js
@@ -21,26 +21,28 @@ let convo = new BotkitConversation(MY_DIALOG_ID, controller)
 //     })
 // })
 
+controller.on('message,direct_message', async(bot, message) => {
+    await bot.reply(message, {text:'暂时不能识别'})
+})
 
 controller.hears(new RegExp('作业'), 'message', async(bot, message) => {
     await bot.beginDialog(MY_DIALOG_ID)
 })
 
 
-// send a greeting
+//发送问候
 convo.say('好的，接下来开始做作业~')
 
-// ask a question, store the response in 'name'
-convo.ask('What is your name?', async(response, convo, bot) => {
-    console.log(`user name is ${ response }`);
-    // do something?
+//添加一个问题，将其存储在'name'里面
+convo.ask('请输入您的名字', async(response, convo, bot) => {
+    console.log(`user name is ${ response }`)
 }, 'name')
 
 // use add action to switch to a different thread, defined below...
 convo.addAction('favorite_color')
 
 // add a message and a prompt to a new thread called `favorite_color`
-convo.addMessage('Awesome {{vars.name}}!', 'favorite_color');
+convo.addMessage('你好，{{vars.name}}！', 'favorite_color')
 convo.addQuestion('Now, what is your favorite color?', async(response, convo, bot) => {
     console.log(`user favorite color is ${ response }`)
 },'color', 'favorite_color')
@@ -53,17 +55,21 @@ convo.addQuestion('Your name is {{vars.name}} and your favorite color is {{vars.
     {
         pattern: 'no',
         handler: async(response, convo, bot) => {
-            // if user says no, go back to favorite color.
             await convo.gotoThread('favorite_color')
         }
     },
     {
         default: true,
         handler: async(response, convo, bot) => {
-            // do nothing, allow convo to complete.
+            console.log(convo.vars)
+            await convo.gotoThread('over')
         }
     }
 ], 'confirm', 'confirmation')
+
+
+convo.addAction('over')
+convo.addMessage('作业结束，辛苦啦~', 'over')
 
 
 controller.addDialog(convo)
