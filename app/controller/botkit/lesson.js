6e096a1051013866646c434fe71abf8af7b75460
hcyhehe <960696163@qq.com>
2020-01-14 23:28:58 +0800

    完善流程

--- app/controller/botkit/lesson.js
+++ app/controller/botkit/lesson.js
@@ -1,5 +1,7 @@
 module.exports = app => {
+    let that = this
     const convoLesson = new app.BotkitConversation(app.DIALOG_ID_LESSON, app.bkController)
+    this.lsn_msg = ''
 
     //查所有教师的课程
     app.bkController.hears(new RegExp('查询所有课程'), 'message', async(bot, message) => {
@@ -14,24 +16,20 @@ module.exports = app => {
 
     //查询单个课程
     app.bkController.hears(new RegExp('查课程'), 'message', async(bot, message) => {
+        that.lsn_msg = message   //提供message上下文
         await bot.beginDialog(app.DIALOG_ID_LESSON)
     })
     convoLesson.say('好的，接下来准备查询课程~')
     convoLesson.ask('请输入您的名字', async(response, convo, bot) => {
+        console.log('输入：', convo.vars.teacher)
         let dbs = await app.db.get('lesson').find({teacher:convo.vars.teacher}).value()
-        console.log('dbs', dbs)
         if(dbs){
             let str = dbs.teacher + ':' + dbs.name + ',' + dbs.weekday + ',' + dbs.times
-            await findOneResult(str)
+            await bot.reply(that.lsn_msg, str)  //使用message上下文做响应
         } else {
-            await findOneResult('查无结果')
+            await bot.reply(that.lsn_msg, '查无结果')
         }
     }, 'teacher')
-
-    convoLesson.addAction('find_one_result')
-    async function findOneResult(str){
-        await convoLesson.addMessage(str, 'find_one_result') 
-    }
 
     app.bkController.addDialog(convoLesson)
 }
