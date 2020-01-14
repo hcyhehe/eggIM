2b6916ff516210184368117346f476fe604d1b4c
hcyhehe <960696163@qq.com>
2020-01-14 19:09:12 +0800

    优化代码，更改整体结构

--- app/controller/botkit/common.js
+++ app/controller/botkit/common.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     app.bkController.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, {text:'暂时不能识别'})
+        await bot.reply(message, {text:'机器人还在学习中，暂时不能识别'})
     })
     
     app.bkController.addDialog(app.convo)
